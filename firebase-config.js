111a2606a717cc8ae626f86e5731a2e6b931af98
manuelitominions-droid <manuelito.minions@gmail.com>
2025-10-09 14:12:02 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
 import { getStorage } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js';
 
-// Configuración Firebase corregida
+// ⚙️ Configuración Firebase correcta (tu proyecto actual)
 const firebaseConfig = {
   apiKey: "AIzaSyAp6lYxU-ZTz8GL4fwAQWbkcCNODQuils8",
-  authDomain: "villa-hermosa-00.firebaseapp.com",
-  projectId: "villa-hermosa-00",
-  storageBucket: "villa-hermosa-00.appspot.com",
+  authDomain: "villa-hermosa-carhuaz-6aa64.firebaseapp.com",
+  projectId: "villa-hermosa-carhuaz-6aa64",
+  storageBucket: "villa-hermosa-carhuaz-6aa64.appspot.com", // 👈 muy importante este cambio
   messagingSenderId: "82074482369",
   appId: "1:82074482369:web:66a1a2cb610b4c21440640",
   measurementId: "G-N8JEN5X0WC"
@@ -21,6 +21,9 @@ export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
 
-// Verificar conexión
+// Hacer accesible globalmente
+window.firebase = { db, auth, storage };
+window.dispatchEvent(new Event('firebaseReady'));
+
 console.log('🔥 Firebase inicializado correctamente para Villa Hermosa');
 console.log('📊 Proyecto:', firebaseConfig.projectId);
